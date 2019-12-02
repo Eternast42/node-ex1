f80ad39a940d9ce3b708dc0733565e56eb549ded
Raphael Cavillon <raphael.cavillon.externe@primonial.fr>
2019-12-02 15:13:53 +0100

    ex3

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ app.get('/', function (req,res) {
 res.send('Hello World')
 })
 
+app.get('/hello', function (req,res) {
+if(!req.query.nom) {
+res.send('Quel est votre nom ?') 
+} else {
+res.send(` bonjour, ${req.query.nom} !`)
+}
+})
+
+
 app.listen(3000, function () {
 console.log('Exemple app listening on port 3000!')
 })
